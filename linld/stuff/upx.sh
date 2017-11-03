b9b9bd732b7164377d05b28763f9d9ce8da2df8a
Pascal Bellard <pascal.bellard@slitaz.org>
2017-11-03 15:13:54 +0100

    linld: updates from rolling

--- linld/stuff/upx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if upx 2>&1 | grep -q "UPX 3.91" ; then
-  upx -5 $1 && echo "0  3C E8 75 FB 89 F7 AD 86 E0 29 F8 AB |" | hexdump -R | \
-  dd bs=1 seek=$(($(stat -c %s $1) - 15)) of=$1 conv=notrunc 2> /dev/null
-else
-  upx -5 --8086 $1
-fi
