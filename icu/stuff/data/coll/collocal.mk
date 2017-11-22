0d5102e12398bd3912b144b3aa766f52c7e2f397
Aleksej Bobylev <al.bobylev@gmail.com>
2017-11-22 21:52:02 +0200

    Update libunwind, strace; pcmanfm-qt: update dependencies; shrink icu (less 2MB comparing to the *-full version); add awf.

---
+++ icu/stuff/data/coll/collocal.mk
@@ -0,0 +1,4 @@
+COLLATION_SOURCE = ar.txt ca.txt cs.txt da.txt de.txt de_AT.txt el.txt en.txt\
+ en_US.txt en_US_POSIX.txt es.txt fa.txt fa_AF.txt fi.txt fr.txt fr_CA.txt\
+ hr.txt hu.txt id.txt is.txt it.txt ja.txt nb.txt nl.txt nn.txt pl.txt pt.txt\
+ ro.txt ru.txt sl.txt sv.txt tr.txt uk.txt vi.txt zh.txt zh_Hant.txt
---
+++ icu/stuff/data/rbnf/rbnflocal.mk
@@ -0,0 +1,5 @@
+RBNF_SOURCE = ar.txt ca.txt cs.txt da.txt de.txt de_CH.txt el.txt en.txt\
+ en_001.txt en_IN.txt es.txt es_419.txt fa.txt fa_AF.txt fi.txt fr.txt\
+ fr_BE.txt fr_CH.txt hr.txt hu.txt id.txt is.txt it.txt ja.txt nb.txt nl.txt\
+ nn.txt pl.txt pt.txt pt_PT.txt ro.txt ru.txt sl.txt sv.txt tr.txt uk.txt\
+ vi.txt zh.txt zh_Hant.txt
